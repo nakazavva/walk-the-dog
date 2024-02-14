e7f71807f5eec6e323954bffefd05008d15410e6
nakazavva <nakazavva@gmail.com>
2024-02-14 23:29:39 +0900

    3.2.4: extract image load fn

--- src/browser.rs
+++ src/browser.rs
@@ -1,8 +1,11 @@
 use anyhow::{anyhow, Result};
 use std::future::Future;
+use wasm_bindgen::closure::{Closure, WasmClosureFnOnce};
 use wasm_bindgen::{JsCast, JsValue};
 use wasm_bindgen_futures::JsFuture;
-use web_sys::{CanvasRenderingContext2d, Document, HtmlCanvasElement, Response, Window};
+use web_sys::{
+    CanvasRenderingContext2d, Document, HtmlCanvasElement, HtmlImageElement, Response, Window,
+};
 
 macro_rules! log {
     ($($t:tt)*) => {
@@ -66,4 +69,15 @@ pub async fn fetch_json(json_path: &str) -> Result<JsValue> {
     )
     .await
     .map_err(|err| anyhow!("error fetching JSON {:#?}", err))
+}
+
+pub fn new_image() -> Result<HtmlImageElement> {
+    HtmlImageElement::new().map_err(|err| anyhow!("Cloud not create HtmlImageElement {:#?}", err))
+}
+
+pub fn closure_once<F, A, R>(fn_once: F) -> Closure<F::FnMut>
+where
+    F: 'static + WasmClosureFnOnce<A, R>,
+{
+    Closure::once(fn_once)
 }
