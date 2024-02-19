3ecbeef6f7cf12ab26084c4b2e32e26ab7b3ee32
nakazavva <nakazavva@gmail.com>
2024-02-19 22:06:37 +0900

    4.3.7.3: update x and y of RHB context

--- src/game.rs
+++ src/game.rs
@@ -69,6 +69,8 @@ mod red_hat_boy_states {
             } else {
                 self.frame = 0;
             }
+            self.position.x += self.velocity.x;
+            self.position.y += self.velocity.y;
             self
         }
     }
